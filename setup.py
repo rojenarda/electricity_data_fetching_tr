2caac437c1ecf2dde1a74e78ddab40b186e640cb
Rojen Arda Şeşen <sesen19@itu.edu.tr>
2024-06-29 21:20:41 +0300

    Version no fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='electricity_data_fetching_tr',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'numpy',
